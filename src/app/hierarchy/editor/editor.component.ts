3a31662f3eaa21bd48b5da970f360a6d20ea471d
sudhir <yadavsudhir405@gmail.com>
2020-07-24 17:26:58 +0530

    Show unassigned propertied on right

---
+++ src/app/hierarchy/editor/editor.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Property} from './propery.interface';
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss']
+})
+export class EditorComponent implements OnInit {
+
+  rootPropertyGroup: FormControl;
+  unassignedProperties: Property[];
+
+  constructor() {
+    this.rootPropertyGroup = new FormControl('', []);
+  }
+
+  ngOnInit(): void {
+    this.initializedUnAssignedProperties();
+  }
+
+  private initializedUnAssignedProperties() {
+    this.unassignedProperties = [
+      {
+        id: 1,
+        name: 'Property1'
+      },
+      {
+        id: 2,
+        name: 'Property2'
+      },
+      {
+        id: 3,
+        name: 'Property3'
+      },
+      {
+        id: 4,
+        name: 'Property4'
+      },
+      {
+        id: 5,
+        name: 'Property5'
+      }
+    ];
+  }
+}
